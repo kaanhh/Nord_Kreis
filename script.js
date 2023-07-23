2cdfa7ce5b3760e24fb5906402bfb1098b6209aa
Kaan Ertugrul <kaan.hh@live.de>
2023-07-23 16:40:26 +0200

    kleiner Text toggle bei Wechsel

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-const bigCircles = document.querySelectorAll('.big-circle'); // Ändern Sie 'querySelector' zu 'querySelectorAll', um alle '.big-circle' Elemente auszuwählen.
+const bigCircles = document.querySelectorAll('.big-circle');
+let activeCircle = null;
 
 bigCircles.forEach(bigCircle => {
   let smallCircles = bigCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2');
-  let smallCircleTexts = bigCircle.querySelectorAll('.kleiner-kreis-text');
+  let smallCircleTexts = bigCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2');
+  let isFirstClick = true; 
 
   bigCircle.addEventListener('click', () => {
-    smallCircles.forEach(circle => circle.style.animationPlayState = 'running');
-    bigCircle.classList.add('clicked'); // Hinzufügen der 'clicked'-Klasse
-  });
+    if (activeCircle === bigCircle) {
+      smallCircleTexts.forEach(text => {
+        text.style.opacity = text.style.opacity === '0' ? '1' : '0';
+      });
+      return; 
+    }
 
-  smallCircles.forEach((circle, index) => {
-    circle.addEventListener('animationend', () => {
-      smallCircleTexts[index].style.opacity = '1';
+    if (activeCircle) {
+      let prevSmallCircles = activeCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2');
+      let prevSmallCircleTexts = activeCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2');
+
+      prevSmallCircles.forEach(circle => {
+        circle.style.animationPlayState = 'paused';
+      });
+
+      prevSmallCircleTexts.forEach(text => {
+        text.style.opacity = '0';
+      });
+
+      activeCircle.classList.remove('clicked');
+    }
+
+    smallCircles.forEach(circle => {
+      circle.style.animation = ''; 
+      circle.style.animationPlayState = 'running';
     });
+
+    smallCircleTexts.forEach(text => {
+      if (!isFirstClick) {
+        text.style.transitionDelay = '0s';
+      }
+      text.style.opacity = '1';
+    });
+
+    bigCircle.classList.add('clicked');
+    activeCircle = bigCircle;
+    isFirstClick = false; 
   });
 });
