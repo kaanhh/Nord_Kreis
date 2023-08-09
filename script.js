e04631f019313326e367655827372614654e70ea
Kaan Ertugrul <kaan.hh@live.de>
2023-08-09 16:51:03 +0200

    Abstand Anpassung 7 Kreise

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let activeCircle = null;
 
 bigCircles.forEach(bigCircle => {
   let smallCircles = bigCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2,.kleiner-kreis_SIEBEN , .kleiner-kreis0_SIEBEN, .kleiner-kreis1_SIEBEN, .kleiner-kreis2_SIEBEN, .kleiner-kreis3_SIEBEN, .kleiner-kreis4_SIEBEN');
-  let smallCircleTexts = bigCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2, .kleiner-kreis-text0, .kleiner-kreis-text1, .kleiner-kreis-text3');
+  let smallCircleTexts = bigCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2, .kleiner-kreis-text0, .kleiner-kreis-text1, .kleiner-kreis-text3, .kleiner-kreis-text4, .kleiner-kreis-text5, .kleiner-kreis-text6, .kleiner-kreis-text7, .kleiner-kreis-text8');
   let isFirstClick = true; 
 
   bigCircle.addEventListener('click', () => {
@@ -17,7 +17,7 @@ bigCircles.forEach(bigCircle => {
 
     if (activeCircle) {
       let prevSmallCircles = activeCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2, .kleiner-kreis_SIEBEN , .kleiner-kreis0_SIEBEN, .kleiner-kreis1_SIEBEN, .kleiner-kreis2_SIEBEN, .kleiner-kreis3_SIEBEN, .kleiner-kreis4_SIEBEN');
-      let prevSmallCircleTexts = activeCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2, .kleiner-kreis-text0, .kleiner-kreis-text1, .kleiner-kreis-text3, .kleiner-kreis-text4');
+      let prevSmallCircleTexts = activeCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2, .kleiner-kreis-text0, .kleiner-kreis-text1, .kleiner-kreis-text3, .kleiner-kreis-text4, .kleiner-kreis-text5, .kleiner-kreis-text6, .kleiner-kreis-text7, .kleiner-kreis-text8');
 
       prevSmallCircles.forEach(circle => {
         circle.style.animationPlayState = 'paused';
