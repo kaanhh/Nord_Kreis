ec78fa765a3dae24d0486e9a605ea8dda8bf2c16
Kaan Ertugrul <kaan.hh@live.de>
2023-08-14 23:11:57 +0200

    a tag nicht mehr anklickbar wenn unsichtbar

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const bigCircles = document.querySelectorAll('.big-circle, .big-circle2');
 let activeCircle = null;
 
 bigCircles.forEach(bigCircle => {
-  let smallCircles = bigCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2,.kleiner-kreis_SIEBEN , .kleiner-kreis0_SIEBEN, .kleiner-kreis1_SIEBEN, .kleiner-kreis2_SIEBEN, .kleiner-kreis3_SIEBEN, .kleiner-kreis4_SIEBEN');
+  let smallCircles = bigCircle.querySelectorAll('.kleiner-kreis, .kleiner-kreis2, .kleiner-kreis_SIEBEN , .kleiner-kreis0_SIEBEN, .kleiner-kreis1_SIEBEN, .kleiner-kreis2_SIEBEN, .kleiner-kreis3_SIEBEN, .kleiner-kreis4_SIEBEN');
   let smallCircleTexts = bigCircle.querySelectorAll('.kleiner-kreis-text, .kleiner-kreis-text2, .kleiner-kreis-text0, .kleiner-kreis-text1, .kleiner-kreis-text3, .kleiner-kreis-text4, .kleiner-kreis-text5, .kleiner-kreis-text6, .kleiner-kreis-text7, .kleiner-kreis-text8');
-  let isFirstClick = true; 
+  let isFirstClick = true;
+
+  // Deaktiviere Links beim Laden der Seite (für mobile Ansicht)
+  smallCircleTexts.forEach(text => {
+    text.querySelector('a').style.pointerEvents = 'none';
+  });
 
   bigCircle.addEventListener('click', () => {
+    if (window.innerWidth < 800) return;  // Unter 800px also Mobil Ansicht
+
     if (activeCircle === bigCircle) {
       smallCircleTexts.forEach(text => {
-        text.style.opacity = text.style.opacity === '0' ? '1' : '0';
-        text.querySelector('a').style.pointerEvents = text.style.opacity === '0' ? 'none' : 'auto';  // Hier setzen wir pointer-events je nach opacity
+        let newOpacity = text.style.opacity === '0' ? '1' : '0';
+        text.style.opacity = newOpacity;
+        text.querySelector('a').style.pointerEvents = newOpacity === '0' ? 'none' : 'auto';
       });
-      return; 
+      return;
     }
 
     if (activeCircle) {
@@ -25,15 +33,21 @@ bigCircles.forEach(bigCircle => {
 
       prevSmallCircleTexts.forEach(text => {
         text.style.opacity = '0';
-        text.querySelector('a').style.pointerEvents = 'none';  // Hier setzen wir pointer-events auf none
+        text.querySelector('a').style.pointerEvents = 'none';
       });
 
       activeCircle.classList.remove('clicked');
     }
 
     smallCircles.forEach(circle => {
-      circle.style.animation = ''; 
+      circle.style.animation = '';
       circle.style.animationPlayState = 'running';
+    });
+
+    circle.addEventListener('animationend', () => {
+      smallCircleTexts.forEach(text => {
+        text.querySelector('a').style.pointerEvents = 'auto';
+      });
     });
 
     smallCircleTexts.forEach(text => {
@@ -41,22 +55,13 @@ bigCircles.forEach(bigCircle => {
         text.style.transitionDelay = '0s';
       }
       text.style.opacity = '1';
-      text.querySelector('a').style.pointerEvents = 'auto';  // Hier setzen wir pointer-events auf auto
     });
 
     bigCircle.classList.add('clicked');
     activeCircle = bigCircle;
-    isFirstClick = false; 
+    isFirstClick = false;
   });
 
   bigCircle.addEventListener('mouseover', () => {
     bigCircle.style.backgroundColor = '#8befd3';
-    bigCircle.style.color = '#e95420'; 
-    
-  });
-
-  bigCircle.addEventListener('mouseout', () => {
-    bigCircle.style.backgroundColor = ''; 
-  });
-});
-
+    bigCircle.style.color = '#e95420
